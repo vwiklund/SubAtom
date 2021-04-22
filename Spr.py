4bbe4489068ff7777f8c9eb553d8b251c71f5eb5
vwiklund <v.wiklund@hotmail.com>
2021-04-22 20:56:38 +0200

    Build Spreadfunction

--- Spr.py
+++ Spr.py
@@ -1,5 +1,20 @@
+import math
 import numpy as np
-
+import scipy.constants as sc
 
 def spread(E, theta, X):
-    return E, theta, X
+    #input
+    E = E
+    theta = theta
+    alpha = 1
+    beta = 1
+
+    #Givet från PM
+    Zp=1
+    Zt=20
+
+    #Fysikaliska konstanter
+    hbar = sc.hbar
+    c = sc.c
+    dSigmadOmega = (Zp**2*Zt**2*alpha**2*(hbar*c)**2)/(4*beta**2*E**2*math.sin(theta/2)**4)
+    return dSigmadOmega
